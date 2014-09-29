ce3150708ab4735699c0b8cc3c602f8f87128e89
profh <profh@cmu.edu>
2014-09-29 16:20:28 -0400

    Added files from lecture on 9/29

---
+++ case_statements/example_of_using_case_statements.sql
@@ -0,0 +1,34 @@
+SELECT name AS "Superhero", to_char(v.for_month,'Month YYYY') AS "Month", (SUM(major) * 3 + SUM(minor)) AS "Score", 
+CASE WHEN (SUM(major) * 3 + SUM(minor)) > 25
+THEN 'Awesome'
+WHEN (SUM(major) * 3 + SUM(minor)) BETWEEN 16 AND 25 
+THEN 'Very good'
+WHEN (SUM(major) * 3 + SUM(minor)) BETWEEN 10 AND 15 
+THEN 'Good; solid'
+WHEN (SUM(major) * 3 + SUM(minor)) BETWEEN 5 AND 9 
+THEN 'Adequate'
+WHEN (SUM(major) * 3 + SUM(minor)) BETWEEN 1 AND 4 
+THEN 'Needs improvement'
+ELSE 'Are you even trying??'
+END AS "Performance"
+FROM victories v JOIN heroes h USING (hero_id)
+GROUP BY name, for_month
+ORDER BY name, for_month;
+
+
+-- SELECT name AS "Superhero", to_char(v.for_month,'Month YYYY') AS "Month", (SUM(major) * 3 + SUM(minor)) AS "Score", 
+-- CASE WHEN "Score" > 25
+-- THEN 'Awesome'
+-- WHEN "Score" BETWEEN 16 AND 25 
+-- THEN 'Very good'
+-- WHEN "Score" BETWEEN 10 AND 15 
+-- THEN 'Good; solid'
+-- WHEN "Score" BETWEEN 5 AND 9 
+-- THEN 'Adequate'
+-- WHEN "Score" BETWEEN 1 AND 4 
+-- THEN 'Needs improvement'
+-- ELSE 'Are you even trying??'
+-- END AS "Performance"
+-- FROM victories v JOIN heroes h USING (hero_id)
+-- GROUP BY name, for_month
+-- ORDER BY name, for_month;
